4793fb37700b1e1c7be43acee4a7b5bb049ad553
Abhilash <abhilash.rpee18@gmail.com>
2023-03-24 18:08:12 +0530

    date picker

--- src/pages/EmployeeDetails.js
+++ src/pages/EmployeeDetails.js
@@ -168,7 +168,7 @@ const EmployeeDetails = (props) => {
     if (month.length < 2) month = "0" + month;
     if (day.length < 2) day = "0" + day;
 
-    return [day, month, year].join("-");
+    return [month, day, year].join("/");
   }
 
   let dd = users[0]?.joining_date;
